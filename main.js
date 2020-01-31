871449a9ce8e2eaddd3986eb8887f4707d2c5ab1
Ewelina Ścisłowska <60173682+ewelinascislowska@users.noreply.github.com>
2020-01-31 10:30:46 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const h1 = document.querySelector('h1');
 document.body.addEventListener("mousemove", function (e) {
-    // h1.textContent = "djsbgksdj"
+
     h1.textContent = e.clientX + ", " + e.clientY;
     document.body.style.backgroundColor = `rgb(${e.clientX / 3}, ${e.clientY / 2}, ${e.clientX / e.clientY * 20})`;
 
